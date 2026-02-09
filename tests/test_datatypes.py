86e15dd0fab4e1f81a0b61d097ca3796229bc431
Jiten Dhandha <jitendhandha@gmail.com>
2026-02-09 10:17:33 +0000

    improve selection funcs and repr's

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -22,11 +22,11 @@ def test_drop_nan():
     assert np.all(np.isfinite(new.data.delta_squared))
 
 
-def test_select_z():
+def test_select_z_range():
     """Test selecting a range of z values from a dataset."""
     dataset = DataSet.load("HERA2025")
     z_val = dataset.data.z[5]
-    selected = dataset.select_z(z_val - 0.1, z_val + 0.1)
+    selected = dataset.select_z_range(z_val - 0.1, z_val + 0.1)
     assert isinstance(selected, DataSet)
     assert np.all(selected.data.z == z_val)
 
@@ -37,20 +37,20 @@ def test_select_k():
     k_val = dataset.data.k[0][5]
     kmin = k_val - 0.1
     kmax = k_val + 0.1
-    selected = dataset.select_k(kmin, kmax)
+    selected = dataset.select_k_range(kmin, kmax)
     assert isinstance(selected, DataSet)
     for k_arr in selected.data.k:
         assert np.all(kmin <= k_arr)
         assert np.all(k_arr <= kmax)
 
 
-def test_select_delta_squared():
+def test_select_delta_squared_range():
     """Test selecting a range of delta_squared values from a dataset."""
     dataset = DataSet.load("HERA2025")
     ds_val = dataset.data.delta_squared[0][5]
     dsmin = ds_val / 10
     dsmax = ds_val * 10
-    selected = dataset.select_delta_sq(dsmin, dsmax)
+    selected = dataset.select_delta_sq_range(dsmin, dsmax)
     assert isinstance(selected, DataSet)
 
     for ds_arr in selected.data.delta_squared:
@@ -66,3 +66,23 @@ def test_select_closest_z():
     assert isinstance(selected, DataSet)
     assert len(selected.data.z) == 1
     assert selected.data.z[0] == z_val
+
+
+def test_select_closest_k():
+    """Test selecting the closest k value from a dataset."""
+    dataset = DataSet.load("HERA2025")
+    k_val = dataset.data.k[0][5]
+    selected = dataset.select_closest_k(k_val)
+    assert isinstance(selected, DataSet)
+    assert len(selected.data.k[0]) == 1
+    assert selected.data.k[0][0] == k_val
+
+
+def test_select_lowest_delta_squared():
+    """Test selecting the lowest delta_squared value from a dataset."""
+    dataset = DataSet.load("HERA2025")
+    selected = dataset.select_lowest_delta_sq()
+    assert isinstance(selected, DataSet)
+    for ds_arr in selected.data.delta_squared:
+        assert len(ds_arr) == 1
+        assert ds_arr[0] == np.nanmin(ds_arr)
