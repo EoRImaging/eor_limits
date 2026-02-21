fd45f7b690e72ca17ab6760e8a2f149906fa95dd
Jiten Dhandha <jitendhandha@gmail.com>
2026-02-21 02:04:02 +0000

    add plot tests

--- tests/test_basic.py
+++
@@ -1,9 +0,0 @@
-"""Really basic tests."""
-
-from eor_limits import make_plot
-
-
-def test_make_plot_basic():
-    """Test making a basic plot with default parameters."""
-    fig = make_plot()
-    assert fig is not None
